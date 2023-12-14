6df134bd8029de083eb75b41217fec863fc6c622
ajr15 <shacharfite@gmail.com>
2023-12-14 17:39:50 +0000

    add porphyrin data

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 COD_SEARCH_API_ENDPOINT = "https://www.crystallography.net/cod/result"
 COD_FILE_API_ENDPOINT = "https://www.crystallography.net/cod/$CODID.cif"
 
-DATA_DIR = "/home/shachar/repos/miscellaneous/CodStructures/data/"
+DATA_DIR = "/home/shachar/repos/CodStructures/data/"
 
 CORROLE_RAW_JSON = DATA_DIR + "corrole_structures.json"
 PORPHYRIN_RAW_JSON = DATA_DIR + "porphyrin_structures.json"
